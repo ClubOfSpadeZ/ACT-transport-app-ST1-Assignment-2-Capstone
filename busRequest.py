69b246bb752458bf3bda31fd08b798111614b9a8
ClubOfSpadeZ <FireHat069@gmail.com>
2023-04-13 17:03:34 +1000

    Ver 1.1.3

--- busRequest.py
+++ busRequest.py
@@ -4,11 +4,10 @@ import dicttoxml
 
 # Make an XML request to the URL
 API_Key = "EA1DEB"
-
 timeStamp = datetime.datetime.now().isoformat()
 
 
-def smRequest(timeStamp, API_Key, busStopID):
+def smRequest(busStopID):
     url = f"http://siri.nxtbus.act.gov.au:11000/{API_Key}/sm/service.xml"
 
     xmlDict = \
@@ -21,14 +20,13 @@ def smRequest(timeStamp, API_Key, busStopID):
     rawXML = dicttoxml.dicttoxml(xmlDict, attr_type=False, custom_root='Siri').decode()
     return rawXML.replace("<Siri>", siriVersionTag), url
 
-def ptRequest(timeStamp, API_Key, line, direction):
+def ptRequest(start, end, line, direction):
     url = f"http://siri.nxtbus.act.gov.au:11000/{API_Key}/pt/service.xml"
 
     xmlDict = \
         {"ServiceRequest": {"RequestTimestamp": timeStamp, "RequestorRef": API_Key,
                             "ProductionTimetableRequest": {"-version": "2.0", "RequestTimestamp": timeStamp,
-                                                      "PreviewInterval": "PT1H", "MonitoringRef": busStopID,
-                                                      "MaximumStopVisits": "4", "MaximumTextLength": "160"}}}
+                                                      "ValidityPeriod": {"StartTime": start, "EndTime": end}, "Lines": {"LineDirection": {"LineRef": line, "DirectionRef": }}}}}
 
     siriVersionTag = '<Siri xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" version="2.0" xmlns="http://www.siri.org.uk/siri">'
     rawXML = dicttoxml.dicttoxml(xmlDict, attr_type=False, custom_root='Siri').decode()
