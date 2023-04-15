d8bf954df490b42841fbe7b37713b8b729030b2e
Rory Birnie <FireHat069@gmail.com>
2023-04-15 17:25:47 +1000

    Ver 1.1.4

--- appGUI.py
+++ appGUI.py
@@ -1,50 +1,53 @@
 import tkinter
+
+
 class myGUI:
     def __init__(self):
         self.main_window = tkinter.Tk()
         self.main_window.title("Pound to Kilogram Calculator")
 
-        #create frames
+        # create frames
         self.input_frame = tkinter.Frame()
         self.button_frame = tkinter.Frame()
         self.result_frame = tkinter.Frame()
 
-        #create label and entry
-        self.Weight_label = tkinter.Label(self.input_frame, text = "Mass in Pounds: ")
+        # create label and entry
+        self.Weight_label = tkinter.Label(self.input_frame, text="Mass in Pounds: ")
         self.Weight_entry = tkinter.Entry(self.input_frame)
-        
-        #create buttons
-        calculate_button = tkinter.Button(self.button_frame, text = "Calculate", command= self.calculate)
-        quit_button = tkinter.Button(self.button_frame, text = "Quit", command= self.main_window.destroy)
 
-        #create text box
+        # create buttons
+        calculate_button = tkinter.Button(self.button_frame, text="Calculate", command=self.calculate)
+        quit_button = tkinter.Button(self.button_frame, text="Quit", command=self.main_window.destroy)
+
+        # create text box
         self.results_tb = tkinter.Text(self.result_frame)
 
-        #pack labels, entry, buttons and text box
+        # pack labels, entry, buttons and text box
         self.Weight_label.pack(side='left')
         self.Weight_entry.pack(side='left')
         calculate_button.pack(side='left')
         quit_button.pack(side='left')
         self.results_tb.pack()
 
-        #pack frames
+        # pack frames
         self.input_frame.pack()
         self.button_frame.pack()
         self.result_frame.pack()
-        
-        #Enter the tkinter main loop
+
+        # Enter the tkinter main loop
         tkinter.mainloop()
 
-    #take user input and display the result
+    # take user input and display the result
     def calculate(self):
         result_string = ""
 
-        #clears the text box every time new entrys are displayed
+        # clears the text box every time new entrys are displayed
         self.results_tb.delete('1.0', tkinter.END)
-        
+
         weight = float(self.Weight_entry.get())
-        weight = weight*0.454
+        weight = weight * 0.454
         result_string = f"Weight = {weight} kg"
         self.results_tb.insert('1.0', result_string)
+
 
 my_gui = myGUI()
--- busRequest.py
+++ busRequest.py
@@ -1,39 +1,125 @@
 import requests
 import datetime
 import dicttoxml
+from typing_extensions import Literal
 
-# Make an XML request to the URL
-API_Key = "EA1DEB"
-timeStamp = datetime.datetime.now().isoformat()
+def build(xmlDict):
+    siriVersionTag = '<Siri xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" version="2.0" xmlns="http://www.siri.org.uk/siri">'
+    rawXML = dicttoxml.dicttoxml(xmlDict, attr_type=False, custom_root='Siri').decode()
+    XML = rawXML.replace("<Siri>", siriVersionTag)
+    return XML
 
 
-def smRequest(busStopID):
-    url = f"http://siri.nxtbus.act.gov.au:11000/{API_Key}/sm/service.xml"
+def URL(ServiceType, ServiceName):
+    url = f"http://siri.nxtbus.act.gov.au:11000/{API_Key}/{ServiceType}/{ServiceName}.xml"
+    return url
+
+
+def timeCONVERT(time_str: str, date_str: str = None):
+    time_obj = datetime.datetime.strptime(time_str, '%I:%M%p').time()
+
+    if date_str is None:
+        today = datetime.datetime.today().date()
+        datetime_obj = datetime.datetime.combine(today, time_obj)
+
+        time_iso = datetime_obj.isoformat()
+        return time_iso
+    else:
+        date = datetime.datetime.strptime(date_str, "%d/%m/%Y").date()
+        datetime_obj = datetime.datetime.combine(date, time_obj)
+
+        time_iso = datetime_obj.isoformat()
+        return time_iso
+
+
+def smRequest(busStopID: int = 0000, previewTime: str = '30M', start: str = ..., direction: Literal['A', 'B'] = ..., stopType: Literal["all", "arrivals", "departures"] = ..., maxTrips: int = ..., debug: bool = False):
+
+    start = timeCONVERT(start)
 
     xmlDict = \
         {"ServiceRequest": {"RequestTimestamp": timeStamp, "RequestorRef": API_Key,
                             "StopMonitoringRequest": {"-version": "2.0", "RequestTimestamp": timeStamp,
-                                                      "PreviewInterval": "PT1H", "MonitoringRef": busStopID,
-                                                      "MaximumStopVisits": "4", "MaximumTextLength": "160"}}}
+                                                      "StartTime": start, "PreviewInterval": f"PT{previewTime}",
+                                                      "MonitoringRef": busStopID, "DirectionRef": direction,
+                                                      "StopVisitTypes": stopType, "MaximumStopVisits": maxTrips,
+                                                      "MaximumTextLength": "1024"}}}
 
-    siriVersionTag = '<Siri xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" version="2.0" xmlns="http://www.siri.org.uk/siri">'
-    rawXML = dicttoxml.dicttoxml(xmlDict, attr_type=False, custom_root='Siri').decode()
-    return rawXML.replace("<Siri>", siriVersionTag), url
+    if start is ...:
+        xmlDict["ServiceRequest"]["StopMonitoringRequest"].pop("StartTime")
+    if direction is ...:
+        xmlDict["ServiceRequest"]["StopMonitoringRequest"].pop("DirectionRef")
+    if stopType is ...:
+        xmlDict["ServiceRequest"]["StopMonitoringRequest"].pop("StopVisitTypes")
+    if maxTrips is ...:
+        xmlDict["ServiceRequest"]["StopMonitoringRequest"].pop("MaximumStopVisits")
 
-def ptRequest(start, end, line, direction):
-    url = f"http://siri.nxtbus.act.gov.au:11000/{API_Key}/pt/service.xml"
+    if debug is True:
+        print(xmlDict)
+        print(build(xmlDict))
+        return
+
+    return requests.post(url=URL(ServiceType="sm", ServiceName="service"), data=build(xmlDict), headers=headers).text
+
+
+def ptRequest(start="2:30pm", end="3:30pm", date: Literal['today', '15/4/2023'] = 'today', line: int = 0, direction: Literal['A', 'B'] = ..., debug: bool = False):
+    f"""
+    This will give you a timetable for a giving time and route for only the day the Request is sent\n
+    argument formats\n
+    start / end: %I:%M%p or HH:MMpm/am I.E 2:30pm\n
+    date: %d/%m/%Y or dd/mm/yyyy I.E 15/4/2023\n
+    direction: 'A' or 'B'\n
+    line: int or '81' or '4'\n
+    the debug argument is used for debuting: ture or false
+    """
+    if date == 'today':
+        start = timeCONVERT(start)
+        end = timeCONVERT(end)
+    else:
+        start = timeCONVERT(start, date)
+        end = timeCONVERT(end, date)
 
     xmlDict = \
         {"ServiceRequest": {"RequestTimestamp": timeStamp, "RequestorRef": API_Key,
                             "ProductionTimetableRequest": {"-version": "2.0", "RequestTimestamp": timeStamp,
-                                                      "ValidityPeriod": {"StartTime": start, "EndTime": end}, "Lines": {"LineDirection": {"LineRef": line, "DirectionRef": }}}}}
+                                                           "ValidityPeriod": {"StartTime": start, "EndTime": end},
+                                                           "Lines": {"LineDirection": {"LineRef": f"ACT_{line}",
+                                                                                       "DirectionRef": direction}}}}}
 
-    siriVersionTag = '<Siri xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" version="2.0" xmlns="http://www.siri.org.uk/siri">'
-    rawXML = dicttoxml.dicttoxml(xmlDict, attr_type=False, custom_root='Siri').decode()
-    return rawXML.replace("<Siri>", siriVersionTag), url
+    if direction is ...:
+        xmlDict["ServiceRequest"]["ProductionTimetableRequest"]["Lines"]["LineDirection"].pop("DirectionRef")
 
-def request(type, ):
-    sm = smRequest(timeStamp, API_Key, busStopID=1231)
+    if debug is True:
+        print(xmlDict)
+        print(build(xmlDict))
+        return
 
-    headers = {'Content-Type': 'application/xml'}  # set what your server accepts
-    print(requests.post(sm[1], data=sm[0], headers=headers).text)
+    return requests.post(url=URL(ServiceType="pt", ServiceName="service"), data=build(xmlDict), headers=headers).text
+
+
+def vmRequest(busID: int = 000, line: int = ..., direction: Literal['A', 'B'] = ..., debug: bool = False):
+    xmlDict = \
+        {"ServiceRequest": {"RequestTimestamp": timeStamp, "RequestorRef": API_Key,
+                            "VehicleMonitoringRequest": {"-version": "2.0", "RequestTimestamp": timeStamp,
+                                                         "VehicleMonitoringRef": f"VM_ACT_{busID}",
+                                                         "LineRef": line, "DirectionRef": direction}}}
+    if line is ...:
+        xmlDict["ServiceRequest"]["VehicleMonitoringRequest"].pop("LineRef")
+    if direction is ...:
+        xmlDict["ServiceRequest"]["VehicleMonitoringRequest"].pop("DirectionRef")
+
+    if debug is True:
+        print(xmlDict)
+        print(build(xmlDict))
+        return
+
+    return requests.post(url=URL(ServiceType="vm", ServiceName="service"), data=build(xmlDict), headers=headers).text
+
+
+if __name__ == '__main__':
+    API_Key = "EA1DEB"
+    timeStamp = datetime.datetime.now().isoformat()
+    headers = {'Content-Type': 'application/xml'}
+
+    print(smRequest(busStopID=2032, previewTime='', direction='A', debug=False))
+    # print(ptRequest(start="6:00am", end="6:15am", line=4, direction='B', debug=False))
+    # print(vmRequest(busID=637, direction='A', debug=False))
