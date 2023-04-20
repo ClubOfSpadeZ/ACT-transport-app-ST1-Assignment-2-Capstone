3e63f68694a30b06ae09989568c07ff916499a5e
u3261101 <125559044+u3261101@users.noreply.github.com>
2023-04-20 17:09:11 +1000

    changed the questions and updated the appGUI file slightly

--- main.py
+++ main.py
@@ -7,11 +7,13 @@
 # Bus Routes XML data from https://data.gov.au/dataset/ds-act-https%3A%2F%2Fwww.data.act.gov.au%2Fapi%2Fviews%2Fifm8-78yv/details?q=action%20bus
 # OPTIONAL NXTBUS real-time data API https://data.gov.au/dataset/ds-act-https%3A%2F%2Fwww.data.act.gov.au%2Fapi%2Fviews%2Fsmqx-37iq/details?q=action%20bus
 # to display the data we will used the google maps python libray and graphs to show travel and wait times
-# Q1: Make an program that displays the travel and wait time between two locations using a time / distance graph (from Lanyon Marketplace to Taylor Adventure Playground)
-# Q2: Display the fastest travel time for Route of Q1
-# Q3: Display the shortest amount of wait time of Route in Q1
-# Q4: Display the least amount of Route change overs
-# Q5: Display the safest route using the Sunday timetable (a safe route will have multiple options to get to a location if a bus is missed or missing)
+'''
+Q1: How long does it take to travel from stop 1 to stop 2
+Q2: What is the fastest travel time for that route out of all options
+Q3: What is shortest wait time between each bus for that route at any time in the day
+Q4: What is the least amount of bus swaps requried to go from stop 1 to stop 2
+Q5: Draw chosen route 
+'''
 # OPTIONAL Q6: Display Bus GPS data on google maps
 # Predictive Analytics: predict the next next bus for a given route if the ideal one is missed
 # OPTIONAL Predictive Analytics: calculate the probability of a Route being either late Early or on-time using the NXTBUS real-time data API
